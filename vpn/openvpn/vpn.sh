38f639ea80426b4cbd6fb8f0b109247eac2def9a
zeroforfun <105130183+zeroforfun@users.noreply.github.com>
2022-05-12 11:37:40 -0700

    Create vpn.sh

---
+++ vpn/openvpn/vpn.sh
@@ -0,0 +1,14 @@
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -I POSTROUTING -s 10.8.0.0/24 -j MASQUERADE
+
+iptables -t nat -D POSTROUTING -s 10.8.0.0/24 -j MASQUERADE
+echo 0 > /proc/sys/net/ipv4/ip_forward
+
+ip route get $server_ip
+ip route add 0.0.0.0/1 via 10.8.0.1
+ip route add 128.0.0.0/1 via 10.8.0.1
+ip route add $server_ip via $server_ip_gateway
+
+ip route add $server_ip
+ip route del 128.0.0.0/1
+ip route del 0.0.0.0/1
