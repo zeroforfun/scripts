2dbd125926cebcd9b7a108a5502f2e335d511da8
zeroforfun <105130183+zeroforfun@users.noreply.github.com>
2022-05-12 11:40:01 -0700

    Update vpn.sh

--- vpn/openvpn/vpn.sh
+++ vpn/openvpn/vpn.sh
@@ -1,8 +1,13 @@
+openvpn --daemon --config $server_conf_path
+
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -I POSTROUTING -s 10.8.0.0/24 -j MASQUERADE
 
 iptables -t nat -D POSTROUTING -s 10.8.0.0/24 -j MASQUERADE
 echo 0 > /proc/sys/net/ipv4/ip_forward
+
+
+openvpn --daemon --config $client_conf_path
 
 ip route get $server_ip
 ip route add 0.0.0.0/1 via 10.8.0.1
