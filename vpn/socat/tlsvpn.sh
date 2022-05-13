b321145340f14fd4b41a62b30bf8b5dd7f98d7c1
zeroforfun <105130183+zeroforfun@users.noreply.github.com>
2022-05-13 10:48:54 -0700

    Update tlsvpn.sh

--- vpn/socat/tlsvpn.sh
+++ vpn/socat/tlsvpn.sh
@@ -1,3 +1,5 @@
+# bash echo ip iptables jq killall openssl ping xargs
+
 openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout /dev/shm/server.key -out /dev/shm/server.crt -subj "/CN=server" 2>/dev/null
 openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout /dev/shm/client.key -out /dev/shm/client.crt -subj "/CN=client" 2>/dev/null
 
