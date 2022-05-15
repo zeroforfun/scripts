72b2c7f9573f210409ac3ef4a2b5944d813ecb30
zeroforfun <105130183+zeroforfun@users.noreply.github.com>
2022-05-15 04:42:38 -0700

    Update openssl.sh

--- openssl/openssl.sh
+++ openssl/openssl.sh
@@ -7,7 +7,7 @@ openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout /dev/shm/client.key 
 openssl req -x509 -utf8 -newkey rsa:4096 -days 365 -addext extendedKeyUsage=serverAuth -nodes -keyout /dev/shm/server.key -out /dev/shm/server.crt -subj "/CN=server" 2>/dev/null
 openssl req -x509 -utf8 -newkey rsa:4096 -days 365 -addext extendedKeyUsage=clientAuth -nodes -keyout /dev/shm/client.key -out /dev/shm/client.crt -subj "/CN=client" 2>/dev/null
 
-openssl rsa -in ./pki/private/cakey.pem -text
-openssl req -in ./pki/reqs/cacsr.pem -text
-openssl x509 -in ./pki/certs/cacert.pem -text
-openssl crl -in ./pki/crl.pem -crlnumber -text
+openssl rsa -text -in ./pki/private/cakey.pem
+openssl req -text -in ./pki/reqs/cacsr.pem
+openssl x509 -text -in ./pki/certs/cacert.pem
+openssl crl -text -crlnumber -in ./pki/crl.pem
