df1a10de54569d9751b6591fbf85963d41a86e86
zeroforfun <105130183+zeroforfun@users.noreply.github.com>
2022-05-15 04:41:52 -0700

    Update openssl.sh

--- openssl/openssl.sh
+++ openssl/openssl.sh
@@ -1,5 +1,5 @@
-openssl req -x509 -newkey rsa:4096 -nodes -keyout /dev/shm/server.key -out /dev/shm/server.crt -subj "/CN=server" 2>/dev/null
-openssl req -x509 -newkey rsa:4096 -nodes -keyout /dev/shm/client.key -out /dev/shm/client.crt -subj "/CN=client" 2>/dev/null
+openssl req -x509 -newkey rsa -nodes -keyout /dev/shm/server.key -out /dev/shm/server.crt -subj "/CN=server" 2>/dev/null
+openssl req -x509 -newkey rsa -nodes -keyout /dev/shm/client.key -out /dev/shm/client.crt -subj "/CN=client" 2>/dev/null
 
 openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout /dev/shm/server.key -out /dev/shm/server.crt -subj "/CN=server" 2>/dev/null
 openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout /dev/shm/client.key -out /dev/shm/client.crt -subj "/CN=client" 2>/dev/null
